185614a0da15232a0e5436a73eeadc1ffff4aac5
Allain Lalonde <allain.lalonde@gmail.com>
2015-10-19 09:40:49 -0400

    Making it specify default which are specific to http

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ var poll = require('./utils').poll;
 var getJson = require('./utils').getJson;
 
 var values = require('object-values');
+var objectAssign = require('object-assign');
 
 module.exports = function (servicifyOptions) {
+  servicifyOptions = objectAssign({
+    host: '127.0.0.1',
+    port: 2020
+  }, servicifyOptions);
+
   return {
     listen: require('./lib/listen')(servicifyOptions),
     offer: require('./lib/offer')(servicifyOptions),
