095f4f1a9cbcafc03e924c4b1395af30f89c3d5c
Allain Lalonde <allain.lalonde@gmail.com>
2015-10-19 10:24:58 -0400

    removing unused requires

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-var Promise = require('native-promise-only');
-var restify = require('restify');
-
-var debug = require('debug')('servicify');
-var poll = require('./utils').poll;
-var getJson = require('./utils').getJson;
-
-var values = require('object-values');
 var objectAssign = require('object-assign');
 
 module.exports = function (servicifyOptions) {
